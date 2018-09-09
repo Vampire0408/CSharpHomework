91aa535ddba60af99cb2c12cd7e663e910a1cc9e
Vampire0408 <437297101@qq,com>
2018-09-09 21:03:55 +0800

    first commit

---
+++ Homework1/第一题：ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int n = 0;
+            int d = 0;
+            Console.WriteLine("Please input an number:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.WriteLine("Please input an number:");
+            s = Console.ReadLine();
+            d = Int32.Parse(s);
+            Console.WriteLine("The product of the two number is " + (n * d));
+        }
+    }
+}
---
+++ Homework1/第二题：WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        public void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            s = textBox1.Text;
+            double m = double.Parse(s);
+            string d = "";
+            d = textBox2.Text;
+            double n = double.Parse(d);
+            double p = m * n;
+            textBox3.Text = m +"*" + n + "=" + p;
+        }
+    }
+}
