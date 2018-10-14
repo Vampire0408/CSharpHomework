07a198c379b42ea7c2058e457899401cf39a2db0
Vampire0408 <437297101@qq.com>
2018-10-15 01:19:55 +0800

    init

---
+++ Homework5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double x2 = x0 + leng * 2 * Math.Cos(th);
+            double y2 = y0 + leng * 2 * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+            drawLine2(x0, y0,x2, y2);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+            drawCayleyTree(n - 1, x2, y2, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Blue,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        void drawLine2(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Green,
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            th1 = double.Parse(s);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            th2 = double.Parse(s);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
